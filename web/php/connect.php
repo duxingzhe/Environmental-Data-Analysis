99fc4de61101c5ace906e01525a5a83e7ca31e54
Lu Xuan <myfather103@163.com>
2019-08-20 09:09:26 +0800

    使用表格显示数据。

--- web/php/connect.php
+++ web/php/connect.php
@@ -20,9 +20,41 @@ $sql = 'select * from environment_record.recorder';
 $result = mysqli_query($mySQLi,$sql);
 if ($result && mysqli_num_rows($result)) {
     // 7、处理数据
-    while ($row = mysqli_fetch_array($result,MYSQLI_NUM)) {
-        var_dump($row);
+    //转成数组，且返回第一条数据,当不是一个对象时候退出
+    echo "<table border='1'>
+        <tr>
+        <th>时间</th>
+        <th>省份</th>
+        <th>城市</th>
+        <th>天气</th>
+        <th>最高温度</th>
+        <th>最低温度</th>
+        <th>空气质量</th>
+        <th>PM2.5</th>
+        <th>二氧化硫</th>
+        <th>二氧化氮</th>
+        <th>一氧化碳</th>
+        <th>预警类型</th>
+        <th>预警等级</th>
+        </tr>";
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>" . $row['time'] . "</td>";
+        echo "<td>" . $row['province'] . "</td>";
+        echo "<td>" . $row['city'] . "</td>";
+        echo "<td>" . $row['weather'] . "</td>";
+        echo "<td>" . $row['highest_temperature'] . "</td>";
+        echo "<td>" . $row['lowest_temperature'] . "</td>";
+        echo "<td>" . $row['air_quality'] . "</td>";
+        echo "<td>" . $row['PM25'] . "</td>";
+        echo "<td>" . $row['SO2'] . "</td>";
+        echo "<td>" . $row['NO2'] . "</td>";
+        echo "<td>" . $row['CO'] . "</td>";
+        echo "<td>" . $row['warning_type'] . "</td>";
+        echo "<td>" . $row['warning_level'] . "</td>";
+        echo "</tr>";
     }
+    echo "</table>";
 } else {
 
 }
