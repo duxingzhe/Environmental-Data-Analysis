0eee17154e6ce10e9dd635ea544b2477ccb7d81c
Lu Xuan <myfather103@163.com>
2019-08-21 09:31:04 +0800

    优化代码流程，确保程序的健壮性。

--- web/php/connect.php
+++ web/php/connect.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "<title>环境数据分析前端</title>";
+
 // Javascript
 echo "<script type=\"text/javascript\">
         function altRows(id){
@@ -64,57 +66,57 @@ if($mySQLi -> connect_errno){
 else
 {
     echo "Success";
-}
 
-// 4、设置字符集
-mysqli_set_charset($mySQLi,'utf8');
-// 5、准备SQL语句
-$sql = 'select * from environment_record.recorder';
-// 6、执行SQL语句
-$result = mysqli_query($mySQLi,$sql);
-if ($result && mysqli_num_rows($result)) {
-    // 7、处理数据
-    //转成数组，且返回第一条数据,当不是一个对象时候退出
-    echo "<table class=\"altrowstable\" id=\"alternatecolor\">
-        <tr align=\"center\" >
-        <th>时间</th>
-        <th>省份</th>
-        <th>城市</th>
-        <th>天气</th>
-        <th>最高温度</th>
-        <th>最低温度</th>
-        <th>空气质量</th>
-        <th>PM2.5</th>
-        <th>二氧化硫</th>
-        <th>二氧化氮</th>
-        <th>一氧化碳</th>
-        <th>预警类型</th>
-        <th>预警等级</th>
-        </tr>";
-    while ($row = mysqli_fetch_array($result)) {
-        echo "<tr align=\"center\" >";
-        echo "<td>" . $row['time'] . "</td>";
-        echo "<td>" . $row['province'] . "</td>";
-        echo "<td>" . $row['city'] . "</td>";
-        echo "<td>" . $row['weather'] . "</td>";
-        echo "<td>" . $row['highest_temperature'] . "</td>";
-        echo "<td>" . $row['lowest_temperature'] . "</td>";
-        echo "<td>" . $row['air_quality'] . "</td>";
-        echo "<td>" . $row['PM25'] . "</td>";
-        echo "<td>" . $row['SO2'] . "</td>";
-        echo "<td>" . $row['NO2'] . "</td>";
-        echo "<td>" . $row['CO'] . "</td>";
-        echo "<td>" . $row['warning_type'] . "</td>";
-        echo "<td>" . $row['warning_level'] . "</td>";
-        echo "</tr>";
+    // 4、设置字符集
+    mysqli_set_charset($mySQLi,'utf8');
+    // 5、准备SQL语句
+    $sql = 'select * from environment_record.recorder';
+    // 6、执行SQL语句
+    $result = mysqli_query($mySQLi,$sql);
+    if ($result && mysqli_num_rows($result)) {
+        // 7、处理数据
+        //转成数组，且返回第一条数据,当不是一个对象时候退出
+        echo "<table class=\"altrowstable\" id=\"alternatecolor\">
+            <tr align=\"center\" >
+            <th>时间</th>
+            <th>省份</th>
+            <th>城市</th>
+            <th>天气</th>
+            <th>最高温度</th>
+            <th>最低温度</th>
+            <th>空气质量</th>
+            <th>PM2.5</th>
+            <th>二氧化硫</th>
+            <th>二氧化氮</th>
+            <th>一氧化碳</th>
+            <th>预警类型</th>
+            <th>预警等级</th>
+            </tr>";
+        while ($row = mysqli_fetch_array($result)) {
+            echo "<tr align=\"center\" >";
+            echo "<td>" . $row['time'] . "</td>";
+            echo "<td>" . $row['province'] . "</td>";
+            echo "<td>" . $row['city'] . "</td>";
+            echo "<td>" . $row['weather'] . "</td>";
+            echo "<td>" . $row['highest_temperature'] . "</td>";
+            echo "<td>" . $row['lowest_temperature'] . "</td>";
+            echo "<td>" . $row['air_quality'] . "</td>";
+            echo "<td>" . $row['PM25'] . "</td>";
+            echo "<td>" . $row['SO2'] . "</td>";
+            echo "<td>" . $row['NO2'] . "</td>";
+            echo "<td>" . $row['CO'] . "</td>";
+            echo "<td>" . $row['warning_type'] . "</td>";
+            echo "<td>" . $row['warning_level'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+
     }
-    echo "</table>";
-} else {
-
+    // 8、释放资源、关闭连接
+    if($result){
+        mysqli_free_result($result);
+    }
+    mysqli_close($mySQLi);
 }
-// 8、释放资源、关闭连接
-if($result){
-    mysqli_free_result($result);
-}
-mysqli_close($mySQLi);
     ?>
