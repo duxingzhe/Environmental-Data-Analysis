483025dfefa0f08dff2c3784c5876e915537f213
Lu Xuan <myfather103@163.com>
2019-08-23 07:28:33 +0800

    加入扇形图测试代码，同时加入PhpInfo函数。

---
+++ web/php/char.php
@@ -0,0 +1,117 @@
+<?php
+Class Chart{
+    private $image; // 定义图像
+    private $title; // 定义标题
+    private $ydata; // 定义Y轴数据
+    private $xdata; // 定义X轴数据
+    private $color; // 定义条形图颜色
+    private $bgcolor; // 定义图片背景颜色
+    private $width; // 定义图片的宽
+    private $height; // 定义图片的长
+
+    /*
+     * 构造函数
+     * String title 图片标题
+     * Array xdata 索引数组，X轴数据
+     * Array ydata 索引数组，数字数组,Y轴数据
+     */
+    function __construct($title,$xdata,$ydata) {
+        $this->title = $title;
+        $this->xdata = $xdata;
+        $this->ydata = $ydata;
+        $this->color = array('#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4');
+    }
+
+    /*
+     * 公有方法，设置条形图的颜色
+     * Array color 颜色数组,元素取值为'#058DC7'这种形式
+     */
+    function setBarColor($color){
+        $this->color = $color;
+    }
+
+    /*
+     * 绘制饼图
+     */
+    function mkPieChart() {
+        $sum = array_sum($this->ydata); // 获取ydata所有元素之和
+        $start = 0; // 弧的开始角度
+        $end = 0; // 弧的结束角度
+        $pieWidth =  300; // 椭圆的长轴
+        $pieHeight = 220; // 椭圆的短轴
+        $space = 40; // 椭圆与小矩形的间距
+        $margin = 20; // 图片的边距
+        $recWidth = 20; // 小矩形的宽
+        $recHeight = 15; // 小矩形的高
+        $titleHeight = 50; // 标题区域的高
+        // 图片自适应宽与高
+        $this->width = $pieWidth + $this->arrayLengthMax($this->xdata)*10*4/3 + $space + $recWidth +$margin;
+        $this->height =  (($pieHeight > count($this->xdata)*25 ) ? $pieHeight : count($this->xdata)*25) + $titleHeight;
+        // 椭圆中心的坐标
+        $cx = $pieWidth/2+$margin;
+        $cy = $pieHeight/2+$titleHeight;
+
+        $this->image = imagecreatetruecolor($this->width ,$this->height); // 准备画布
+        $this->bgcolor = imagecolorallocate($this->image,255,255,255); // 图片的背景颜色
+        imagefill($this->image,0,0,$this->bgcolor); // 填充背景
+
+        // 设置条形图的颜色
+        $color = array();
+        foreach($this->color as $col) {
+            $col = substr($col,1,strlen($col)-1);
+            $red = hexdec(substr($col,0,2));
+            $green = hexdec(substr($col,2,2));
+            $blue = hexdec(substr($col,4,2));
+            $color[] = imagecolorallocate($this->image ,$red, $green, $blue);
+        }
+
+        // 设置线段的颜色、字体的颜色、字体的路径
+        $lineColor = imagecolorallocate($this->image ,0xcc,0xcc,0xcc);
+        $fontColor = imagecolorallocate($this->image, 0x95,0x8f,0x8f);
+        $fontPath = 'font/simsun.ttc';
+
+        // 绘制扇形弧
+        for($i = 0; $i < 10; $i++) {
+            foreach($this->ydata as $key => $val) {
+                $end += 360*$val/$sum;
+                imagefilledarc($this->image,$cx,$cy-$i,$pieWidth,$pieHeight, $start,$end,$color[$key%count($this->color)],IMG_ARC_PIE);
+                $start = $end;
+            }
+        }
+
+        // 绘制小矩形及之后文字说明
+        $x1 = $pieWidth+$space;
+        $y1 = $titleHeight ;
+        foreach($this->ydata as $key => $val) {
+            imagefilledrectangle($this->image,$x1,$y1,$x1+$recWidth,$y1+$recHeight,$color[$key%count($this->color)]);
+            imagettftext($this->image,10,0,$x1+$recWidth+5,$y1+$recHeight-2,$fontColor,$fontPath,$this->xdata[$key]);
+            $y1 += $recHeight + 10;
+        }
+
+        // 绘画标题
+        $titleStart = ($this->width - 5.5*strlen($this->title))/2;
+        imagettftext($this->image,11,0,$titleStart,20,$fontColor,$fontPath,$this->title);
+
+        // 输出图片
+        header("Content-Type:image/png");
+        imagepng($this->image);
+    }
+
+    /*
+     * 私有方法，求数组中元素长度最大的值
+     * Array arr 字符串数组，必须是汉字
+     */
+    private function arrayLengthMax($arr) {
+        $length = 0;
+        foreach($arr as $val) {
+            $length = strlen($val) > $length ? strlen($val) : $length;
+        }
+        return $length/3;
+    }
+
+    // 析构函数
+    function __destruct(){
+        imagedestroy($this->image);
+    }
+}
+?>
--- web/php/test.php
+++ web/php/test.php
@@ -1,3 +1,22 @@
 <?php
-echo "Hello";
+$image=imagecreatetruecolor(220,220); //制作画布
+//是指图像中所需的颜色，相当画画是准备的染料
+$white=imagecolorallocate($image,0xff,0xff,0xff);
+$darkgray=imagecolorallocate($image,0xc0,0xc0,0xc0);
+$gray=imagecolorallocate($image,0x90,0x90,0x90);
+$navy=imagecolorallocate($image,0x00,0x00,0x80);
+$darknavy=imagecolorallocate($image,0x00,0x00,0x50);
+$red=imagecolorallocate($image,0xff,0x00,0x00);
+$darkred=imagecolorallocate($image,0x90,0x00,0x00);
+imagefill($image,0,0,$white);
+
+imagefilledarc($image,100,110,200,200,-160,40,$darknavy,IMG_ARC_PIE);
+imagefilledarc($image,100,110,200,200,40,75,$darkgray,IMG_ARC_PIE);
+imagefilledarc($image,100,110,200,200,75,200,$darkred,IMG_ARC_PIE);
+
+imagestring($image,10,35,135,'34.7%',$white);
+imagestring($image,10,85,65,'55.5%',$white);
+header('Content-type:image/png');
+imagepng($image);
+imagedestroy($image);
 ?>
