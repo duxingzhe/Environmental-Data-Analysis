49b0784276846381ad2aca9d24ac9beb12c42334
Lu Xuan <myfather103@163.com>
2019-09-01 08:55:17 +0800

    开始连接数据库。

--- desktop/Visualization/main.cpp
+++ desktop/Visualization/main.cpp
@@ -1,9 +1,27 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+
+//连接数据库
+bool createConnection()
+{
+    QSqlDatabase db;
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("F:\\Project\\Environment\\database\\environmentDBForQt.db");
+    if(!db.open())
+    {
+        return false;
+    }
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    if(!createConnection())
+        return 1;
     MainWindow w;
     w.show();
 
