72814118277463b06b2f84e622232fdf8177366b
luxuan <myfather103@163.com>
2019-09-29 15:36:52 +0800

    练习cartopy包。

--- script/data-analysis/test.py
+++ script/data-analysis/test.py
@@ -1,88 +1,60 @@
-from mpl_toolkits.axisartist.parasite_axes import HostAxes, ParasiteAxes
-import matplotlib.pyplot as plt
 import numpy as np
+import skimage.io
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 
-fig = plt.figure(1) #定义figure，（1）中的1是什么
-ax_cof = HostAxes(fig, [0, 0, 0.9, 0.9])  #用[left, bottom, weight, height]的方式定义axes，0 <= l,b,w,h <= 1
+# Image has shape: (y: 296, x: 640, RGBA: 4)
+atw80d = skimage.io.imread('./640px-Around_the_World_in_Eighty_Days_map.png')
 
-#parasite addtional axes, share x
-ax_temp = ParasiteAxes(ax_cof, sharex=ax_cof)
-ax_load = ParasiteAxes(ax_cof, sharex=ax_cof)
-ax_cp = ParasiteAxes(ax_cof, sharex=ax_cof)
-ax_wear = ParasiteAxes(ax_cof, sharex=ax_cof)
+yellowish = ((atw80d[:, :, 0] > 200) &  # Lots of Red.
+             (atw80d[:, :, 1] > 200) &  # Lots of Green.
+             (atw80d[:, :, 2] < 100) &  # Not lots of Blue.
+             (atw80d[:, :, 3] > 250))   # Not transparent.
 
-#append axes
-ax_cof.parasites.append(ax_temp)
-ax_cof.parasites.append(ax_load)
-ax_cof.parasites.append(ax_cp)
-ax_cof.parasites.append(ax_wear)
+ind_y, ind_x = np.where(yellowish)
+print('Number of yellow-ish pixels: ', len(ind_x))
+print('x indexes: ', ind_x)
+print('y indexes: ', ind_y)
 
+def pixels_to_data(extent, shape, i, j):
+    """Converts from coordinates of the array to data coordinates."""
+    xmin, xmax, ymin, ymax = extent
+    x_range = xmax - xmin
+    y_range = ymax - ymin
 
+    pix_width = x_range / shape[1]
+    pix_height = y_range / shape[0]
 
-#invisible right axis of ax_cof
-ax_cof.axis['right'].set_visible(False)
-ax_cof.axis['top'].set_visible(False)
-ax_temp.axis['right'].set_visible(True)
-ax_temp.axis['right'].major_ticklabels.set_visible(True)
-ax_temp.axis['right'].label.set_visible(True)
+    # For y handle the fact that the image's pixels
+    # start at the top (in mpl that is what origin='upper' means).
+    j = shape[0] - j
 
-#set label for axis
-ax_cof.set_ylabel('cof')
-ax_cof.set_xlabel('Distance (m)')
-ax_temp.set_ylabel('Temperature')
-ax_load.set_ylabel('load')
-ax_cp.set_ylabel('CP')
-ax_wear.set_ylabel('Wear')
+    return (xmin + pix_width * (i + 0.5),
+            ymin + pix_height * (j + 0.5))
 
-load_axisline = ax_load.get_grid_helper().new_fixed_axis
-cp_axisline = ax_cp.get_grid_helper().new_fixed_axis
-wear_axisline = ax_wear.get_grid_helper().new_fixed_axis
+# Use the extents computed in an earlier exercise.
+extent = [-13636707, 17044670, -6308712, 8565930]
 
-ax_load.axis['right2'] = load_axisline(loc='right', axes=ax_load, offset=(40,0))
-ax_cp.axis['right3'] = cp_axisline(loc='right', axes=ax_cp, offset=(80,0))
-ax_wear.axis['right4'] = wear_axisline(loc='right', axes=ax_wear, offset=(120,0))
+# Note: Data coords in Robinson.
+xs, ys = pixels_to_data(
+    extent, atw80d.shape,
+    ind_x, ind_y)
 
-fig.add_axes(ax_cof)
+print('Last 3 x coordinates (Robinson): ', xs[-3:])
+print('Last 3 y coordinates (Robinson): ', ys[-3:])
 
-''' #set limit of x, y
-ax_cof.set_xlim(0,2)
-ax_cof.set_ylim(0,3)
-'''
+plt.figure()
 
-curve_cof, = ax_cof.plot([0, 1, 2], [0, 1, 2], label="CoF", color='black')
-curve_temp, = ax_temp.plot([0, 1, 2], [0, 3, 2], label="Temp", color='red')
-curve_load, = ax_load.plot([0, 1, 2], [1, 2, 3], label="Load", color='green')
-curve_cp, = ax_cp.plot([0, 1, 2], [0, 40, 25], label="CP", color='pink')
-curve_wear, = ax_wear.plot([0, 1, 2], [25, 18, 9], label="Wear", color='blue')
+rob = ccrs.Robinson(central_longitude=11.25)
+ax = plt.axes(projection=rob)
 
+ax.gridlines(color='gray', linestyle='--')
+ax.coastlines()
+ax.imshow(atw80d, extent=extent,
+          transform=rob, origin='upper')
+ax.set_global()
 
-ax_temp.set_ylim(0,4)
-ax_load.set_ylim(0,4)
-ax_cp.set_ylim(0,50)
-ax_wear.set_ylim(0,30)
-
-ax_cof.legend()
-
-#轴名称，刻度值的颜色
-#ax_cof.axis['left'].label.set_color(ax_cof.get_color())
-ax_temp.axis['right'].label.set_color('red')
-ax_load.axis['right2'].label.set_color('green')
-ax_cp.axis['right3'].label.set_color('pink')
-ax_wear.axis['right4'].label.set_color('blue')
-
-ax_temp.axis['right'].major_ticks.set_color('red')
-ax_load.axis['right2'].major_ticks.set_color('green')
-ax_cp.axis['right3'].major_ticks.set_color('pink')
-ax_wear.axis['right4'].major_ticks.set_color('blue')
-
-ax_temp.axis['right'].major_ticklabels.set_color('red')
-ax_load.axis['right2'].major_ticklabels.set_color('green')
-ax_cp.axis['right3'].major_ticklabels.set_color('pink')
-ax_wear.axis['right4'].major_ticklabels.set_color('blue')
-
-ax_temp.axis['right'].line.set_color('red')
-ax_load.axis['right2'].line.set_color('green')
-ax_cp.axis['right3'].line.set_color('pink')
-ax_wear.axis['right4'].line.set_color('blue')
-
+plt.plot(xs, ys, transform=rob,
+         linestyle='none', marker='o',
+         markeredgecolor='k', color='yellow')
 plt.show()
