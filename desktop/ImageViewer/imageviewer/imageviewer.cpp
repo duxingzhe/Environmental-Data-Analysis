e1844965487be57b864cb8c41ec096e3703c33e2
Lu Xuan <myfather103@163.com>
2019-10-10 10:48:19 +0800

    添加图片查看代码。

--- desktop/ImageViewer/imageviewer/imageviewer.cpp
+++ desktop/ImageViewer/imageviewer/imageviewer.cpp
@@ -1,6 +1,6 @@
 #include "imageviewer.h"
 
-Imageviewer::Imageviewer()
+Imageviewer::ImageViewer()
 {
 
 }
--- desktop/ImageViewer/imageviewer/imageviewer.h
+++ desktop/ImageViewer/imageviewer/imageviewer.h
@@ -1,11 +1,62 @@
 #ifndef IMAGEVIEWER_H
 #define IMAGEVIEWER_H
 
+#include <QWidget>
+#include <QImage>
+#include <QPixmap>
+#include <QDir>
+#include <QSize>
+#include <QFileInfo>
+#include <QFileInfoList>
 
-class Imageviewer
+class ImageViewer : public QWidget
 {
+    Q_OBJECT
 public:
-    Imageviewer();
+    explicit ImageViewer(QWidget *parent=0);
+    explicit ImageViewer(QWidget *parent, QString &caption, QString &dir, QString &filer);
+    ~ImageViewer();
+
+    QWidget *parent;
+
+    int index;
+    int angle;
+    QSize size;
+    QString filename;
+    QString path;
+    QDir dir;
+    QFileInfo fileInfo;
+    QFileInfoList fileInfoList;
+
+    QImage image;
+    QPixmap;
+
+    int openImageFile(const QString &caption, const QString &dir, const QString &filer);
+
+    int closeImageFile(void);
+
+    int delImageFile(void);
+
+    int last(void);
+    int next(void);
+
+    int zoomIn(void);
+    int zoomOut(void);
+    int spinToRight(void);
+    int spinToLeft(void);
+
+private:
+
+    void initImageResource(void);
+
+    int loadImageResource(void);
+    int loadImageResource(const QString &caption, const QString &dir, const QString &filer);
+
+    int getFileInfoList(void);
+    int getFileCurIndex(void);
+    int upgradeFileInfo(QString &filename, int angle, int sizeScale);
+
+public slots:
 };
 
 #endif // IMAGEVIEWER_H
