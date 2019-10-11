4b343ce09f5c467f5dc0701fc8be07e75e82b88f
Lu Xuan <myfather103@163.com>
2019-10-11 10:40:44 +0800

    提交查看器代码。

--- desktop/ImageViewer/imageviewer/imageviewer.cpp
+++ desktop/ImageViewer/imageviewer/imageviewer.cpp
@@ -1,6 +1,60 @@
 #include "imageviewer.h"
 
-Imageviewer::ImageViewer()
-{
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QDebug>
+#include <QImageReader>
 
+ImageViewer::ImageViewer(QWidget *parent) : QWidget(parent)
+{
+    this->parent=parent;
+    initImageResource();
+}
+
+ImageViewer:ImageViewer(QWidget *parent, QString &caption, QString &dir,
+                        QString &filer)
+{
+    this->parent=parent;
+    initImageResource();
+    loadImageResource(caption, dir, filer);
+}
+
+ImageViewer::~ImageViewer(void)
+{
+    this->parent=NULL;
+}
+
+int ImageViewer::openImageFile(const QString &caption, const QString &dir,
+                               const QString &filer)
+{
+    initImageResource();
+    return loadImageResource(caption, dir, file);
+}
+
+int ImageViewer::closeImageFile(void)
+{
+    initImageResource();
+    return 0;
+}
+
+int ImageViewer::delImageFile(void)
+{
+    if(filename.isEmpty())
+    {
+        return -1;
+    }
+
+    if(QFile::remove(filename))
+    {
+        qDebug()<<"remove success: "<<filename;
+    }
+    else
+    {
+        qDebug()<<"remove failed: "<<filename;
+        return -1;
+    }
+
+    fileInfoList.removeAt(index);
+
+    return 0;
 }
