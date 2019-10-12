794e66d1faa6f03b75d770b31f9bd75bb43005dc
Lu Xuan <myfather103@163.com>
2019-10-12 08:54:50 +0800

    添加查看器代码。

--- desktop/ImageViewer/mainwindow/mainwindow.h
+++ desktop/ImageViewer/mainwindow/mainwindow.h
@@ -2,6 +2,15 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <Qmenu>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QStatusBar>
+#include <QLabel>
+#include <QFileInfoList>
+
+#include "qabout.h"
+#include "imageviewer.h"
 
 class MainWindow : public QMainWindow
 {
@@ -9,9 +18,45 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
 
-signals:
+private:
+    QMenuBar *menuBar;
+    QToolBar *toolBar;
+    QWidget *centralWidget;
+    QStatusBar *statusBar;
+    QLabel *imageLabel;
+    QAbout aboutWidget;
+
+    ImageViewer *imageViewer;
+
+    void initMainWidnow(void);
+    void initUiComponent(void);
+    void setImageViewerWidget(void);
+    void initImageResource(void);
+    void loadImageResource(void);
+
+    QAction *openAction;
+    QAction *closeAction;
+    QAction *lastAction;
+    QAction *nextAction;
+    QAction *toLeftAction;
+    QAction *toRightAction;
+    QAction *toEnlargeAction;
+    QAction *toLessenAction;
+    QAction *deleteAction;
 
 public slots:
+
+    void openActionTriggered(void);
+    void closeActionTriggered(void);
+    void lastActionTriggered(void);
+    void nextActionTriggered(void);
+    void toLeftActionTriggered(void);
+    void toRightActionTriggered(void);
+    void toEnlargeActionTriggered(void);
+    void toLessenActionTriggered(void);
+    void deleteActionTriggered(void);
+    void aboutTriggered(void);
+    void aboutQtTriggered(void);
 };
 
 #endif // MAINWINDOW_H
