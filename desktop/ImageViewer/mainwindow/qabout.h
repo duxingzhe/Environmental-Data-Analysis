7778dd02d65df659b06b344a8347ed4e84f43fe6
Lu Xuan <myfather103@163.com>
2019-10-13 10:35:14 +0800

    添加查看器代码。

--- desktop/ImageViewer/mainwindow/qabout.h
+++ desktop/ImageViewer/mainwindow/qabout.h
@@ -2,16 +2,32 @@
 #define QABOUT_H
 
 #include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QTextEdit>
+
+#define QABOUT_WIDGET_WIDTH 320
+#define QABOUT_WIDGET_HEIGHT 240
 
 class QAbout : public QWidget
 {
     Q_OBJECT
 public:
     explicit QAbout(QWidget *parent = nullptr);
+    ~QAbout();
+
+private:
+    QPushButton *exitButton;
+    QLabel *infoLabel;
+    QLabel *titleLabel;
+    QLabel *authorLabel;
+    QTextEdit *infoTextEdit;
+    void initUiComponent(void);
 
 signals:
 
 public slots:
+    void exitButtonClicked(void);
 };
 
 #endif // QABOUT_H
