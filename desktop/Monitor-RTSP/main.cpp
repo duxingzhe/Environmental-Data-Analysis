4d3030c46389af77a0df298d6ca64994d7596917
Lu Xuan <myfather103@163.com>
2019-10-20 07:55:46 +0800

    添加监视器代码。

--- desktop/Monitor-RTSP/main.cpp
+++ desktop/Monitor-RTSP/main.cpp
@@ -1,10 +1,14 @@
 #include "mainwindow.h"
-
+#include <QTextCodec>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8"); //设置编码格式为UTF-8
+    QTextCodec::setCodecForLocale(codec);
+
     MainWindow w;
     w.show();
     return a.exec();
