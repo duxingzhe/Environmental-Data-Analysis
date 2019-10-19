5cdce05c0612d90d9ec1930056ba5c7b2d67c1da
Lu Xuan <myfather103@163.com>
2019-10-20 07:59:48 +0800

    添加监视器代码。

--- desktop/Monitor-RTSP/videoplayer.h
+++ desktop/Monitor-RTSP/videoplayer.h
@@ -1,11 +1,36 @@
 #ifndef VIDEOPLAYER_H
 #define VIDEOPLAYER_H
 
+#include <QThread>
+#include <QImage>
 
-class VideoPlayer
+class VlcInstance;
+class VlcMedia;
+class VlcMediaPlayer;
+
+class VideoPlayer : public QThread
 {
+    Q_OBJECT
+
 public:
-    VideoPlayer();
+    explicit VideoPlayer();
+    ~VideoPlayer();
+
+    void startPlay();
+
+signals:
+    void sig_GetOnFrame(QImage);
+    void sig_GetRFrame(QImage);
+
+protected:
+    void run();
+
+private:
+    QString mFileName;
+
+    VlcInstance *_instance;
+    VlcMedia *_media;
+    VlcMediaPlayer *_player;
 };
 
 #endif // VIDEOPLAYER_H
