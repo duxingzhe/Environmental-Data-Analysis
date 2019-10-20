9f0fe2812941fe21c1645d459b0994091ee01aff
Lu Xuan <myfather103@163.com>
2019-10-20 11:29:27 +0800

    添加监视器代码。

--- desktop/Monitor-RTSP/mainwindow.h
+++ desktop/Monitor-RTSP/mainwindow.h
@@ -2,10 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QImage>
+#include <QPaintEvent>
+#include <QWidget>
+#include <QtDebug>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+#include <QtConcurrent/qtconcurrentrun.h>
+#include "videoplayer.h"
+
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
@@ -15,7 +22,26 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+
+    void paintEvent(QPaintEvent *event);
+
 private:
     Ui::MainWindow *ui;
+
+    VideoPlayer *mPlayer;
+
+    QImage mImage;
+    QImage R_mImage;
+
+    QString url;
+
+    bool open_red=false;
+
+private slots:
+    void slotGetOneFrame(QImage img);
+    void slotGetRFrame(QImage img);
+    bool slotOpenRed();
+    bool slotCloseRed();
 };
 #endif // MAINWINDOW_H
