99d36fa2da4b37bea99c55c52866620790c0afc0
Lu Xuan <myfather103@163.com>
2019-10-20 08:16:46 +0800

    添加监视器代码。

--- desktop/Monitor-RTSP/videoplayer.cpp
+++ desktop/Monitor-RTSP/videoplayer.cpp
@@ -69,4 +69,56 @@ void VideoPlayer::run()
     }
 
     videoStream=-1;
+
+    for(i=0;i<pFormatCtx->nb_streams;i++)
+    {
+        if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO)
+        {
+            videoStream=i;
+        }
+    }
+
+    if(videoStream==-1)
+    {
+        printf("Didn't find a video stream.\n");
+        return;
+    }
+
+    pCodecCtx=pFormatCtx->streams[videoStream]->codec;
+    pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
+    pCodecCtx->bit_rate=0;
+    pCodecCtx->time_base.num=1;
+    pCodecCtx->time_base.den=0;
+    pCodecCtx->frame_number=1;
+
+    if(pCodec==NULL)
+    {
+        printf("Codec not found.\n");
+        return;
+    }
+
+    if(avcodec_open2(pCodecCtx, pCodec, NULL)<0)
+    {
+        printf("Could not open codec.\n");
+        return;
+    }
+
+    pFrame=av_frame_alloc();
+    pFrameRGB=av_frame_alloc();
+
+    img_convert_ctx=sws_getContext(pCodecCtx->width, pCodecCtx->height,
+                                   pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height,
+                                   AV_PIX_FMT_RGB32, SWS_BICUBIC, NULL, NULL, NULL);
+
+    numBytes=avpicture_get_size(AV_PIX_FMT_RGB32, pCodecCtx->width, pCodecCtx->height);
+
+    out_buffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
+    avpicture_fill((AVPicture *)pFrameRGB, out_buffer, AV_PIX_FMT_RGB32,
+                   pCodecCtx->width, pCodecCtx->height);
+
+    int y_size=pCodecCtx->width *pCodecCtx->height;
+
+    packet=(AVPacket *)malloc(sizeof(AVPacket));
+    av_new_packet(packet, y_size);S
+
 }
