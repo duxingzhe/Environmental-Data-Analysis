1b276f3bb75b02d3d92d6e1f4bcb9720801cb8e3
Lu Xuan <myfather103@163.com>
2019-10-24 10:36:50 +0800

    添加网络请求代码。

---
+++ mobile/Android/HttpRequest/app/src/main/java/com/luxuan/httprequest/OkhttpUtil.java
@@ -0,0 +1,47 @@
+package com.luxuan.httprequest;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OkhttpUtil {
+
+    protected OkHttpClient mOkHttpClient;
+    private static OkhttpUtil okhttpUtil;
+    private OkhttpUtil(){
+
+    }
+
+    public static OkhttpUtil getOkhttpUtil(){
+        if(okhttpUtil==null) {
+            okhttpUtil = new OkhttpUtil();
+        }
+        return okhttpUtil;
+    }
+    //初始化okHttpClient
+    public void initOkHttpClient() {
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS);
+        mOkHttpClient = builder.build();
+    }
+
+    //请求方法
+    public String getResponse(String cityName) throws IOException{
+        //请求组合创建
+        Request request = new Request.Builder()
+                .url("https://www.environment.com/php/test.php?key="+Utils.API+"&cityname="+cityName)
+                .build();
+
+        //发起请求
+        Response response;
+        response = mOkHttpClient.newCall(request).execute();
+        return response.body().string();
+    }
+
+}
