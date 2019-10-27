db0ae16b1fd1a14955c912e26cc7312a7e00c20e
Lu Xuan <myfather103@163.com>
2019-10-27 20:54:44 +0800

    添加RTMP推流代码。

--- mobile/Android/RTMPPusher/app/src/main/cpp/AndroidLog.h
+++ mobile/Android/RTMPPusher/app/src/main/cpp/AndroidLog.h
@@ -2,7 +2,13 @@
 // Created by Luxuan on 2019/10/26.
 //
 
+#pragma once
 #ifndef RTMPPUSHER_ANDROIDLOG_H
 #define RTMPPUSHER_ANDROIDLOG_H
+
+#include <android/log.h>
+
+#define LOGD(FORMAT,...) __android_log_print(ANDROID_LOG_DEBUG, "luxuan", FORMAT, ##__VA_ARGS__);
+#define LOGE(FORMAT,...) __android_log_print(ANDROID_LOG_ERROR, "luxuan", FORMAT, ##__VA_ARGS__);
 
 #endif //RTMPPUSHER_ANDROIDLOG_H
