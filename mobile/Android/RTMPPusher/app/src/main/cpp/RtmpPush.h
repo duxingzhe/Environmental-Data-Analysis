6829cb9855d5d19f0b7a41e68d480c432421b7e1
Lu Xuan <myfather103@163.com>
2019-10-30 09:41:45 +0800

    添加RTMP推流代码。

--- mobile/Android/RTMPPusher/app/src/main/cpp/RtmpPush.h
+++ mobile/Android/RTMPPusher/app/src/main/cpp/RtmpPush.h
@@ -5,4 +5,38 @@
 #ifndef RTMPPUSHER_RTMPPUSH_H
 #define RTMPPUSHER_RTMPPUSH_H
 
+#include <malloc.h>
+#include <string.h>
+#include "LxQueue.h"
+#include "phtread.h"
+#inlcude "LxCallaJava.h"
+
+extern "C"
+{
+#include "librtmp/rtmp.h"
+};
+
+class RtmpPush
+{
+
+public:
+    RTMP *rtmp=NULL;
+    char *url=NULL;
+    LxQueue *queue=NULL;
+    pthread_t push_thread;
+    LxCallJava *lxCallJava=NULL;
+    bool startPushing=false;
+    long startTime=0;
+
+public:
+    RtmpPush(const char *url, LxCallJava *lxCallJava);
+    ~RtmpPush();
+
+    void init();
+    void pushSPSPPS(char *sps, int sps_len, char *pps, int pps_len);
+    void pushVideoData(char *data, int data_len, bool keyframe);
+    void pushAudioData(char *data, int data_len);
+    void pushStop();
+};
+
 #endif //RTMPPUSHER_RTMPPUSH_H
