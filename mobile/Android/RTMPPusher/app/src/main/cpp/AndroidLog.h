60cdca2a2ad646fa457c457cb08d1b0824abd637
Lu Xuan <myfather103@163.com>
2019-11-01 07:10:35 +0800

    添加RTMP推流代码。

--- mobile/Android/RTMPPusher/app/src/main/cpp/AndroidLog.h
+++ mobile/Android/RTMPPusher/app/src/main/cpp/AndroidLog.h
@@ -8,6 +8,8 @@
 
 #include <android/log.h>
 
+#define LOG_SHOW true
+
 #define LOGD(FORMAT,...) __android_log_print(ANDROID_LOG_DEBUG, "luxuan", FORMAT, ##__VA_ARGS__);
 #define LOGE(FORMAT,...) __android_log_print(ANDROID_LOG_ERROR, "luxuan", FORMAT, ##__VA_ARGS__);
 
--- mobile/Android/RTMPPusher/app/src/main/cpp/Lxpush.cpp
+++ mobile/Android/RTMPPusher/app/src/main/cpp/Lxpush.cpp
@@ -25,3 +25,27 @@ Java_com_luxuan_pusher_push_LxPushVideo_initPush(JNIEnv *env, jobject instance, 
 
     env->ReleaseStringUTFChars(pushUrl, pushUrl_);
 }
+
+extern "C"
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
+{
+    javaVM=vm;
+    JNIEnv* env;
+    if(vm->GetEnv((void **)&env, JNI_VERSION_1_4)!=JNI_OK)
+    {
+        if(LOG_SHOW)
+        {
+            LOGE("GetEnv failed!");
+        }
+
+        return -1;
+    }
+
+    return JNI_VERSION_1_4;
+}
+
+extern "C"
+JNIEXPORT void JNICALL JNI_OnUnload(JavaVM* vm, void* reserved)
+{
+    javaVM=NULL;
+}
