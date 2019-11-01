08c91bc377a745cd1f6a634fffb4cfc9813fa833
Lu Xuan <myfather103@163.com>
2019-11-01 14:50:32 +0800

    添加RTMP推流代码。

--- mobile/Android/RTMPPusher/app/src/main/java/com/luxuan/rtmppusher/push/LxPushVideo.java
+++ mobile/Android/RTMPPusher/app/src/main/java/com/luxuan/rtmppusher/push/LxPushVideo.java
@@ -2,6 +2,34 @@ package com.luxuan.rtmppusher.push;
 
 public class LxPushVideo {
 
+    private LxConnectListener lxConnectListener;
+
+    static {
+        System.loadLibrary("lxpush");
+    }
+
+    public void setLxConnectListener(LxConnectListener lxConnectListener){
+        this.lxConnectListener=lxConnectListener;
+    }
+
+    private void onConnecting(){
+        if(lxConnectListener!=null){
+            lxConnectListener.onConnecting();
+        }
+    }
+
+    private void onConnectSuccess(){
+        if(lxConnectListener!=null){
+            lxConnectListener.onConnectSuccess();
+        }
+    }
+
+    private void onConnectFailed(String msg){
+        if(lxConnectListener!=null){
+            lxConnectListener.onConnectFailed(msg);
+        }
+    }
+
     public void stopPush(){
         pushStop();
     }
