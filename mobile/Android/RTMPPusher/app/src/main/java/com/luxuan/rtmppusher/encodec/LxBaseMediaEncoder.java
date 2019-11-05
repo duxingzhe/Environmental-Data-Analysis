b36d53d5f23f671b404c7241708859cee3358d45
Lu Xuan <myfather103@163.com>
2019-11-05 09:31:52 +0800

    添加RTMP推流代码。

--- mobile/Android/RTMPPusher/app/src/main/java/com/luxuan/rtmppusher/encodec/LxBaseMediaEncoder.java
+++ mobile/Android/RTMPPusher/app/src/main/java/com/luxuan/rtmppusher/encodec/LxBaseMediaEncoder.java
@@ -8,6 +8,8 @@ import android.view.Surface;
 
 import com.luxuan.rtmppusher.egl.LXEGLSurfaceView;
 
+import java.lang.ref.WeakReference;
+
 import javax.microedition.khronos.egl.EGLContext;
 
 public abstract class LxBaseMediaEncoder {
@@ -49,4 +51,58 @@ public abstract class LxBaseMediaEncoder {
 
     }
 
+    public void setRender(LXEGLSurfaceView.LxGLRender lxGLREnder){
+        this.lxGLRender=lxGLRender;
+    }
+
+    public void setRenderMode(int mRenderMode){
+        if(lxGLRender==null){
+            throw new RuntimeException("must set render before");
+        }
+        this.mRenderMode=mRenderMode;
+    }
+
+    public void setOnMediaInfoListener(OnMediaInfoListener onMediaInfoListener){
+        this.onMediaInfoListener=onMediaInfoListener;
+    }
+
+    public void initEncodec(EGLContext eglContext, String savePath, int width, int height, int sampleRate, int channelCount){
+        this.width=width;
+        this.height=height;
+        this.eglContext=eglContext;
+        initMediaEncode(savePath, width, height, sampleRate, channelCount);
+    }
+
+    public void startRecord(){
+        if(surface!=null && eglContext != null){
+            audioPts=0;
+            audioExit=false;
+            audioExit=false;
+            encodecStart=false;
+
+            lxEGLMediaThread=new LxEncodecMediaThread(new WeakReference<LxBaseMediaEncoder>(this));
+            videoEncodecThread=new VideoEncodecThread(new WeakReference<LxBaseMediaEncoder>(this));
+            audioEncodecThread=new VideoEncodecThread(new WeakReference<LxBaseMediaEncoder>(this));
+            lxEGLMediaThread.isCreate=true;
+            lxEGLMediaThread.isChange=true;
+            videoEncodecThread.start();
+            audioEncodecThread.start();
+        }
+    }
+
+    public void stopRecord(){
+        if(lxEGLMediaThread!=null&&videoEncodecThread!=null&&audioEncodecThread!=null){
+            videoEncodecThread.exit();
+            audioEncodecThread.exit();
+            lxEGLMediaThread.onDestroy();
+            videoEncodecThread=null;
+            lxEGLMediaThread=null;
+            audioEncodecThread=null;
+        }
+    }
+
+    public interface OnMediaInfoListener{
+
+        void onMediaTime(int times);
+    }
 }
