6acc4d07494e004dde772dc29d2b0c9ec1300627
Lu Xuan <myfather103@163.com>
2019-11-09 10:43:31 +0800

    添加RTMP推流代码。

--- mobile/Android/RTMPPusher/app/src/main/java/com/luxuan/rtmppusher/camera/LxCameraFboRender.java
+++ mobile/Android/RTMPPusher/app/src/main/java/com/luxuan/rtmppusher/camera/LxCameraFboRender.java
@@ -102,4 +102,38 @@ public class LxCameraFboRender {
 
         bitmapTextureId=LxShaderUtil.loadBitmapTexture(bitmap);
     }
+
+    public void onChange(int width, int height){
+        GLES20.glViewport(0,0, width,height);
+    }
+
+    public void onDraw(int textureId){
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glClearColor(1f, 0f, 0f, 1f);
+
+        GLES20.glUseProgram(program);
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
+
+        GLES20.glEnableVertexAttribArray(vPosition);
+        GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8, 0);
+
+        GLES20.glEnableVertexAttribArray(fPosition);
+        GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8, vertexData.length);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, bitmapTextureId);
+
+        GLES20.glEnableVertexAttribArray(vPosition);
+        GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8, 32);
+
+        GLES20.glEnableVertexAttribArray(fPosition);
+        GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8, vertexData.length);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+    }
 }
