f3ede00a9b58e654b4032811bf40772fb8eb1555
luxuan <myfather103@163.com>
2019-11-11 22:44:07 +0800

    Add MySQL code.

--- desktop/MySQL/MySQL/main.cpp
+++ desktop/MySQL/MySQL/main.cpp
@@ -1,10 +1,24 @@
 #include "mainwindow.h"
-
+#include <QSqlDatabase>
+#include <QDebug>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSqlDatabase m_db;
+    m_db = QSqlDatabase::addDatabase("QMYSQL");
+    m_db.setHostName("127.0.0.1");
+    m_db.setPort(3306);
+    m_db.setDatabaseName("environment");
+    m_db.setUserName("root");
+    m_db.setPassword("");
+    if(!m_db.open()){
+        qDebug() << "error";
+        return 0;
+    }else{
+        qDebug() << "Success";
+    }
     MainWindow w;
     w.show();
     return a.exec();
