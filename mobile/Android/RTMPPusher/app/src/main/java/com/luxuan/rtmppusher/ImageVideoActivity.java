fc40a6b67cef52ec967abcbe252dbc0b010c5ac0
Lu Xuan <myfather103@163.com>
2019-11-12 09:02:56 +0800

    添加RTMP推流代码。

---
+++ mobile/Android/RTMPPusher/app/src/main/java/com/luxuan/rtmppusher/ImageVideoActivity.java
@@ -0,0 +1,11 @@
+package com.luxuan.rtmppusher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.luxuan.rtmppusher.imgVideo.LxImgVideoView;
+
+public class ImageVideoActivity extends AppCompatActivity {
+
+    private LxImgVideoView lxImgVideoView;
+    private LxMediaEncodec
+}
