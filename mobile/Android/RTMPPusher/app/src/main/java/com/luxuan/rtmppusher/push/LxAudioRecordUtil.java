71e5de3516984da8ee5ca4c44e7781477ec4fea9
Lu Xuan <myfather103@163.com>
2019-11-13 09:08:10 +0800

    添加RTMP推流代码。

--- mobile/Android/RTMPPusher/app/src/main/java/com/luxuan/rtmppusher/push/LxAudioRecordUtil.java
+++ mobile/Android/RTMPPusher/app/src/main/java/com/luxuan/rtmppusher/push/LxAudioRecordUtil.java
@@ -1,4 +1,61 @@
 package com.luxuan.rtmppusher.push;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
 public class LxAudioRecordUtil {
+
+    private AudioRecord audioRecord;
+    private int bufferSizeInBytes;
+    private boolean start=false;
+    private int readSize=0;
+
+    private OnRecordListener onRecordListener;
+
+    public LxAudioRecordUtil(){
+        bufferSizeInBytes=AudioRecord.getMinBufferSize(
+                44100, AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+
+        audioRecord=new AudioRecord(MediaRecorder.AudioSource.MIC,
+                44100, AudioFormat.CHANNEL_IN_STEREO,AudioFormat.ENCODING_PCM_16BIT,
+                bufferSizeInBytes);
+    }
+
+    public void startRecord(){
+        new Thread(){
+
+            @Override
+            public void run(){
+                super.run();
+                start=true;
+                audioRecord.startRecording();
+                byte[] audioData=new byte[bufferSizeInBytes];
+                while(start){
+                    readSize=audioRecord.read(audioData, 0, bufferSizeInBytes);
+                    if(onRecordListener!=null){
+                        onRecordListener.recordByte(audioData, readSize);
+                    }
+                }
+
+                if(audioRecord!=null){
+                    audioRecord.stop();
+                    audioRecord.release();
+                    audioRecord=null;
+                }
+            }
+        }.start();
+    }
+
+    public void stopRecord(){
+        start=false;
+    }
+
+    public interface OnRecordListener{
+        void recordByte(byte[] audioData, int readSize);
+    }
+
+    public boolean isStart(){
+        return start;
+    }
 }
