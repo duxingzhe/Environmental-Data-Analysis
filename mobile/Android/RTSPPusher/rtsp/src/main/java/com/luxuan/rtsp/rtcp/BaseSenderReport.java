4774442260979042286c86b7062154c018991ddc
Lu Xuan <myfather103@163.com>
2019-11-16 08:42:55 +0800

    添加RTSP代码。

--- mobile/Android/RTSPPusher/rtsp/src/main/java/com/luxuan/rtsp/rtcp/BaseSenderReport.java
+++ mobile/Android/RTSPPusher/rtsp/src/main/java/com/luxuan/rtsp/rtcp/BaseSenderReport.java
@@ -1,5 +1,7 @@
 package com.luxuan.rtsp.rtcp;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Random;
 
 public abstract class BaseSenderReport {
@@ -7,7 +9,7 @@ public abstract class BaseSenderReport {
     protected static final String TAG="BaseSenderReport";
     protected static final int PACKET_LENGTH=28;
     private static final int MTU=1500;
-    private final long itnerval=3000;
+    private final long interval=3000;
 
     private final byte[] videoBuffer=new byte[MTU];
     private final byte[] audioBuffer=new byte[MTU];
@@ -32,5 +34,82 @@ public abstract class BaseSenderReport {
 
         setLong(videoBuffer, new Random().nextInt(), 4, 8);
         setLong(audioBuffer, new Random().nextInt(), 4, 8);
+    }
+
+    public static BaseSenderReport getInstance(Protocol protocol, int videoSourcePort, int audioSourcePort){
+        return protocol==Protocol.TCP?new SenderReportTcp(): new SenderReportUdp(videoSourcePort, audioSourcePort);
+    }
+
+    public abstract void setDataStream(OutputStream outputStream, String host);
+
+    public void update(RtpFrame rtpFrame){
+        if(rtpFrame.getChannelIdentifier()==(byte)2){
+            updateVideo(rtpFrame);
+        }else{
+            updateAudio(rtpFrame);
+        }
+    }
+
+    public abstract void sendReport(byte[] buffer, RtpFrame rtpFrame, String type, int packetCount, int octetCount) throws IOException;
+
+    private void updateVideo(RtpFrame rtpFrame){
+        videoPacketCount++;
+        videoOctetCount+=rtpFrame.getLength();
+        setLong(videoBuffer, videoPacketCount, 20, 24);
+        setLong(videoBuffer, videoOctetCount, 24, 28);
+
+        if(System.currentTimeMillis()-videoTime>=interval){
+            videoTime=System.currentTimeMillis();
+            setData(videoBuffer, System.nanoTime(), rtpFrame.getTimeStamp());
+            try{
+                sendReport(videoBuffer, rtpFrame, "Video", videoPacketCount, videoOctetCount);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void updateAudio(RtpFrame rtpFrame){
+        audioPacketCount++;
+        audioOctetCount+=rtpFrame.getLength();
+        setLong(audioBuffer, audioPacketCount, 20, 24);
+        setLong(audioBuffer, audioOctetCount, 24, 28);
+
+        if(System.currentTimeMillis()-audioTime>=interval){
+            audioTime=System.currentTimeMillis();
+            setData(audioBuffer, System.nanoTime(), rtpFrame.getTimeStamp());
+            try{
+                sendReport(audioBuffer, rtpFrame, "Video", audioPacketCount, audioOctetCount);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void reset(){
+        videoPacketCount=videoOctetCount=0;
+        audioPacketCount=audioOctetCount=0;
+        videoTime=audioTime=0;
+        setLong(videoBuffer, videoPacketCount, 20, 24);
+        setLong(videoBuffer, videoOctetCount, 24, 28);
+        setLong(audioBuffer, audioPacketCount, 20, 24);
+        setLong(audioBuffer, audioOctetCount, 24, 28);
+    }
+
+    public abstract void close();
+
+    private void setLong(byte[] buffer, long n, int begin, int end){
+        for(end--;end>=begin;end--){
+            buffer[end]=(byte)(n%256);
+            n>>=8;
+        }
+    }
+
+    private void setData(byte[] buffer, long ntpts, long rtpts){
+        long hb=ntpts/1000000000;
+        long lb=((ntpts-hb*1000000000)*4294967296L)/1000000000;
+        setLong(buffer, hb, 8, 12);
+        setLong(buffer, lb, 12, 16);
+        setLong(buffer, rtpts, 16, 20);
     }
 }
