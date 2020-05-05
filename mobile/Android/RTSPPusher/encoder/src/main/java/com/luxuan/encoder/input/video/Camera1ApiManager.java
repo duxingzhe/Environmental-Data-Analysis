33b6cfe1cf150086d0d72010cae5435b41551c8a
Lu Xuan <myfather103@163.com>
2020-05-05 19:38:10 +0800

    添加RTSP代码。

--- mobile/Android/RTSPPusher/encoder/src/main/java/com/luxuan/encoder/input/video/Camera1ApiManager.java
+++ mobile/Android/RTSPPusher/encoder/src/main/java/com/luxuan/encoder/input/video/Camera1ApiManager.java
@@ -1,11 +1,15 @@
 package com.luxuan.encoder.input.video;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.SurfaceView;
 import android.view.TextureView;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Camera1ApiManager {
@@ -28,6 +32,7 @@ public class Camera1ApiManager {
     private int height=480;
     private int fps=30;
     private int rotation=0;
+    private int imageFormat= ImageFormat.NV21;
     private byte[] yuvBuffer;
     private List<Camera.Size> previewSizeBack;
     private List<Camera.Size> previewSizeFront;
@@ -87,7 +92,7 @@ public class Camera1ApiManager {
         this.height=height;
         this.fps=fps;
         this.cameraFacing=facing;
-        cameraSelect=facing==Camera.CameraInfo.CAMERA_FACING_BACK?slectCameraBack():selectCameraFront();
+        cameraSelect=facing==Camera.CameraInfo.CAMERA_FACING_BACK?selectCameraBack():selectCameraFront();
         start();
     }
 
@@ -99,6 +104,71 @@ public class Camera1ApiManager {
     }
 
     private void start(){
+        if(!checkCanOpen()){
+            throw new CameraOpenException("This camera resolution can't be opened");
+        }
+        yuvBuffer=new byte[width*height*3/2];
+        try{
+            camera=Camera.open(cameraSelect);
+            Camera.CameraInfo info=new Camera.CameraInfo();
+            Camera.getCameraInfo(cameraSelect, info);
+            isFrontCamera=info.facing==Camera.CameraInfo.CAMERA_FACING_FRONT;
+            isPortrait=context.getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_PORTRAIT;
+            Camera.Parameters parameters=camera.getParameters();
+            parameters.setPreviewSize(width, height);
+            parameters.setPreviewFormat(imageFormat);
+            int[] range=adaptFpsRange(fps, parameters.getSupportedPreviewFpsRange());
+            parameters.setPreviewFpsRange(range[0], range[1]);
 
+            List<String> supportedFocusModes=parameters.getSupportedFocusModes();
+            if(supportedFocusModes!=null&&!supportedFocusModes.isEmpty()){
+                if(supportedFocusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)){
+                    parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+                }else if(supportedFocusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)){
+                    parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+                }else{
+                    parameters.setFocusMode(supportedFocusModes.get(0));
+                }
+            }
+            camera.setParameters(parameters);
+            camera.setDisplayOrientation(rotation);
+            if(surfaceView!=null){
+                camera.setPreviewDisplay(surfaceView.getHolder());
+                camera.addCallbackBuffer(yuvBuffer);
+                camera.setPreviewCallbackWithBuffer(this);
+            }else if(textureView!=null){
+                camera.setPreviewTexture(textureView.getSurfaceTexture());
+                camera.addCallbackBuffer(yuvBuffer);
+                camera.setPreviewCallbackWithBuffer(this);
+            }else{
+                camera.setPreviewTexture(surfaceTexture);
+            }
+            camera.startPreview();
+            running=true;
+            Log.i(TAG, width+"X"+height);
+        }catch(IOException e){
+            Log.e(TAG, "Error", e);
+        }
+    }
+
+    private int selectCameraBack(){
+        return selectCamera(Camera.CameraInfo.CAMERA_FACING_BACK);
+    }
+
+    private int selectCameraFront(){
+        return selectCamera(Camera.CameraInfo.CAMERA_FACING_FRONT);
+    }
+
+    private int selectCamera(int facing){
+        int number=Camera.getNumberOfCameras();
+        for(int i=0;i<number;i++){
+            Camera.CameraInfo info=new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if(info.facing==facing){
+                return i;
+            }
+        }
+        return 0;
     }
 }
