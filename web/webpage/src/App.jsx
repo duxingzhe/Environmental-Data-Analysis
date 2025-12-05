ebcd16fdacc0345a61754334a323444bc114706c
luxuan <myfather103@163.com>
2025-12-05 23:43:29 +0800

    升级AntD到6.0.0

--- web/webpage/src/App.jsx
+++ web/webpage/src/App.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
-import { Button } from 'antd';
+import {Button, Flex, Table} from 'antd';
+
+const dataSource = [
+  {
+    key: "1",
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号',
+  },
+  {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号',
+  },
+];
+
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
 
 const App = () => (
     <div className="App">
-        <Button type="primary">Button</Button>
+      <Flex gap="small" wrap>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+      </Flex>
+      <Table dataSource={dataSource} columns={columns} />;
     </div>
 );
 
