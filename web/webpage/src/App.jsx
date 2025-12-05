a98a2fca4ebda168e6a8acb3ad8df90f229a1614
luxuan <myfather103@163.com>
2025-12-05 23:11:05 +0800

    升级AntD到6.0.0

---
+++ web/webpage/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const App = () => (
+    <div className="App">
+        <Button type="primary">Button</Button>
+    </div>
+);
+
+export default App;
